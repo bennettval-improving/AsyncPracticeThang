4808da9ed7d1d44777ff2db95c53a7592d2e7c5a
Bennett Valentine <bennett.valentine@improving.com>
2021-06-04 08:30:26 -0500

    Initial Commit 🔥

---
+++ AsyncPracticeThang/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AsyncPracticeThang.Models;
+using AsyncPracticeThang.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AsyncPracticeThang.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var randomNumber = await GetRandomNumber();
+            var viewModel = new IndexViewModel { RandomNumber = randomNumber };
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task<string> GetRandomNumber()
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://seriouslyfundata.azurewebsites.net/api/generatearandomnumber");
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
+    }
+}
---
+++ AsyncPracticeThang/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncPracticeThang.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public string RandomNumber { get; set; }
+    }
+}
